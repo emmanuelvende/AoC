e393d4badd78790f368fe8d592c9018723f6e79b
emmanuelvende <emmanuel@vende.fr>
2022-12-08 14:07:59 +0100

    in progress :(

--- day_08/p1.py
+++ day_08/p1.py
@@ -12,77 +12,95 @@ for line in lines:
     input_line = line[:-1]
     row = [x for x in input_line]
     rows.append(row)
-
-# for row in rows:
-#     print(row)
-# print(len(rows[0]))
-# print(len(rows))
-
-
-
-def is_visible(x, rest_of_trees, mini):
-    if len(rest_of_trees) == 0:
-        return x > mini
-    else:
-        return max(rest_of_trees) <= x and x > mini
-
-
-def get_column_from_up(rows, i):
-    return [row[i] for row in rows]
-
-
-def get_column_from_down(rows, i):
-    return list(reversed(get_column_from_up(rows, i)))
-
-
-def get_row_from_left(rows, i):
-    return rows[i]
-
-
-def get_row_from_right(rows, i):
-    return list(reversed(rows[i]))
-
-
-def count(u, mini):
-    nb = 0
-    for i, x in enumerate(u):
-        if is_visible(x, u[i + 1 :], mini):
-            nb += 1
-            break
-    return nb
-
-
-def count_from_up(rows, i):
-    column = get_column_from_up(rows, i)
-    return count(column, column[0])
-
-
-def count_from_down(rows, i):
-    column = get_column_from_down(rows, i)
-    return count(column, column[0])
-
-
-def count_from_left(rows, i):
-    row = get_row_from_left(rows, i)
-    return count(row, row[0])
-
-
-def count_from_right(rows, i):
-    row = get_row_from_right(rows, i)
-    return count(row, row[0])
-
-
-
 a = len(rows)
 
-for i in range(1, a-1):
-    print(f"{i=} --> {count_from_up(rows, i)=}")
-    print(f"{i=} --> {count_from_right(rows, i)=}")
-    print(f"{i=} --> {count_from_down(rows, i)=}")
-    print(f"{i=} --> {count_from_left(rows, i)=}")
 
-# nb_edges = 4 * a - 3 
-nb_edges = 4 * a - 3 - 4
+def is_visible(x, first_trees, edge):
+    if len(first_trees) == 0:
+        return x > edge
+    else:
+        return x > edge and max(first_trees) < x
+
+
+def get_column_from_up(rows, col):
+    return [row[col] for row in rows]
+
+
+def get_column_from_down(rows, col):
+    return list(reversed(get_column_from_up(rows, col)))
+
+
+def get_row_from_left(rows, row):
+    return rows[row]
+
+
+def get_row_from_right(rows, row):
+    return list(reversed(rows[row]))
+
+
+# print("=== TEST === get_column_from_up")
+# for i in range(a):
+#     print(f"{i=} --> {get_column_from_up(rows, col=i)=}")
+
+# print("=== TEST === get_column_from_down")
+# for i in range(a):
+#     print(f"{i=} --> {get_column_from_down(rows, col=i)=}")
+
+# print("=== TEST === get_row_from_left")
+# for i in range(a):
+#     print(f"{i=} --> {get_row_from_left(rows, row=i)=}")
+
+# print("=== TEST === get_row_from_right")
+# for i in range(a):
+#     print(f"{i=} --> {get_row_from_right(rows, row=i)=}")
+
+
+def check_visibility(trees):
+    nb = 0
+    edge = trees[0]
+    rank = 0
+
+    inside_trees = trees[1:-1]
+    for i, x in inside_trees[1:]:
+        pass
+
+    # inside_trees = trees[1:-1]
+    # for i, x in enumerate(inside_trees):
+    #     if is_visible(x, inside_trees[:i], edge):
+    #         nb += 1
+    #         rank = i
+    #         break
+    return nb, rank
+
+
+def detect_from_up(rows, col):
+    column = get_column_from_up(rows, col)
+    return check_visibility(column)
+
+
+def detect_from_down(rows, i):
+    column = get_column_from_down(rows, i)
+    return check_visibility(column)
+
+
+def detect_from_left(rows, i):
+    row = get_row_from_left(rows, i)
+    return check_visibility(row)
+
+
+def detect_from_right(rows, i):
+    row = get_row_from_right(rows, i)
+    return check_visibility(row)
+
+
+# for i in range(1, a - 1):
+#     print(f"{i=} --> {detect_from_up(rows, i)=}")
+#     print(f"{i=} --> {detect_from_right(rows, i)=}")
+#     print(f"{i=} --> {detect_from_down(rows, i)=}")
+#     print(f"{i=} --> {detect_from_left(rows, i)=}")
+
+# nb_edges = 4 * a - 3
+nb_edge_trees = 4 * (a - 1)
 
 # for i in range(a):
 #     print(f"{i=} --> {get_column_from_up(rows, i)=}")
@@ -90,13 +108,36 @@ nb_edges = 4 * a - 3 - 4
 # for i in range(a):
 #     print(f"{i=} --> {get_column_from_down(rows, i)=}")
 
-total = 0
-for i in range(1, a-1):
-    total += count_from_up(rows, i)
-    total += count_from_right(rows, i)
-    total += count_from_down(rows, i)
-    total += count_from_left(rows, i)
+detected_trees = set()
 
-total += nb_edges
+for i in range(a):
+    detected_trees.add((i, 0))
+    detected_trees.add((i, a - 1))
+    detected_trees.add((0, i))
+    detected_trees.add((a - 1, i))
+print(f"nb of edge trees : {len(detected_trees)}")
 
-print(total)
+for i in range(a):
+    up_visibility, rank = detect_from_up(rows, i)
+    if up_visibility >= 1:
+        col, row = i, rank
+        detected_trees.add((col, row))
+
+    right_visibility, rank = detect_from_right(rows, i)
+    if right_visibility >= 1:
+        col, row = a - 1 - rank, i
+        detected_trees.add((col, row))
+
+    down_visibility, rank = detect_from_down(rows, i)
+    if down_visibility >= 1:
+        col, row = i, a - 1 - rank
+        detected_trees.add((col, row))
+
+    left_visibility, rank = detect_from_left(rows, i)
+    if left_visibility >= 1:
+        col, row = rank, i
+        detected_trees.add((col, row))
+
+print(f"{detected_trees=}")
+
+print(f"{len(detected_trees)=}")
