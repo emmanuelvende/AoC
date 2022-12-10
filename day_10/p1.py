e0f7108be75607de186cac0c0bfaff76718e1ee4
Emmanuel Vendé perso <47533717+emmanuelvende@users.noreply.github.com>
2022-12-10 13:22:33 +0100

    embedding x register in the Device class

--- day_10/p1.py
+++ day_10/p1.py
@@ -13,28 +13,28 @@ NTH_TIMES_TO_CONSIDER = [20, 60, 100, 140, 180, 220]
 class Device:
     def __init__(self):
         self.time = 0
+        self.x = 1
         self.signal_strength = 0
 
-    def tick(self, x):
+    def tick(self):
         self.time += 1
         if self.time in NTH_TIMES_TO_CONSIDER:
-            self.signal_strength += self.time * x
+            self.signal_strength += self.time * self.x
 
 
 device = Device()
-x = 1
 
 for line in lines:
     args = line[:-1].split()
     if len(args) == 1:
         op = args[0]
     else:
-        op, val = args
+        op, val = args[0], int(args[1])
     if op == "noop":
-        device.tick(x)
+        device.tick()
     elif op == "addx":
-        device.tick(x)
-        device.tick(x)
-        x += int(val)
+        device.tick()
+        device.tick()
+        device.x += val
 
 print(device.signal_strength)
